d6ed30564dd031d508024ba555f6b7e7d1196ea2
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-07 18:31:47 +0200

    Delete when_added field and add unique=True to name field

--- medicines/models.py
+++ medicines/models.py
@@ -16,14 +16,12 @@ class Medicines(models.Model):
         VITAMIN = 'VIT', _('Vitamin')
         SUPLEMENT = 'SUP', _('Suplement')
 
-    name = models.CharField(verbose_name='Name', max_length=120)
+    name = models.CharField(verbose_name='Name', max_length=120, unique=True)
     description = models.TextField(verbose_name='Description')
     med_form = models.CharField(
         verbose_name='Form', max_length=3, choices=MedicineForm.choices, default=MedicineForm.TABLETS, )
     med_type = models.CharField(
         verbose_name='Type', max_length=3, choices=MedicineType.choices, default=MedicineType.VITAMIN, )
-    when_added = models.DateTimeField(
-        verbose_name='When added', auto_now_add=True)
 
     def __str__(self):
         return self.name
