2bed574411450f17b6338b6255635c19539643fa
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-10 13:11:26 +0200

    Create model for doctors

--- doctors/models.py
+++ doctors/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Doctors(models.Model):
+    class DoctorSpecialization(models.TextChoices):
+        UROLOGIS = 'URO', _('Urologis')
+        ORTHOPEDIST = 'ORT', _('Orthopedist')
+        OPHTHALMOLOGIST = 'OPH', _('Ophthalmologist')
+        NEUROLOGIST = 'NEU', _('Neurologist')
+        SURGEON = 'SUR', _('Surgeon')
+        LARYNGOLOGIST = 'LAR', _('Laryngologist')
+        GYNECALOGIST = 'GYN', _('Gynecologist')
+        FAMILY_DOCTOR = 'FDO', _('Family doctor')
+        CARDIOLOGIST = 'CAR', _('Cardiologist')
+        ONCOLOGIST = 'ONC', _('Oncologist')
+        GASTROENTEROLOGIST = 'GAS', _('Gastroenterologist')
+        ENDYCORNOLOGIST = 'END', _('Endocrinologist.')
+        DERMATOLOGIST = 'DER', _('Dermatologist')
+        ALLERGIST = 'ALL', _('Allergist')
+
+    name = models.CharField(verbose_name='Name', max_length=120, unique=True)
+    adres = models.CharField(verbose_name='Adres', max_length=200)
+    phone_number = models.CharField(
+        verbose_name='Phone Number', max_length=12, unique=True, blank=True, null=True)
+    doc_type = models.CharField(
+        verbose_name='Specialization', max_length=3, choices=DoctorSpecialization.choices, default=DoctorSpecialization.FAMILY_DOCTOR)
+
+    def __str__(self):
+        return self.name
