407e380db845946dc2436ac1a1bd62d942d71c68
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-13 09:46:19 +0200

    Create onw UserAdmin class inherited from orginal class

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
 
-admin.site.register(User)
+class UserAdmin(BaseUserAdmin):
+    """Override UserAdmin class to use own ordering and field display"""
+    ordering = ['id']
+    list_display = ['name', 'email']
+
+
+admin.site.register(User, UserAdmin)
