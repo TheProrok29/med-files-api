5aecc575dec95de296a2c9a9ae799fcb8356f14a
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-15 16:20:37 +0200

    Add users and rest_framework.authtoken to INSTALLED_APPS

--- med_files_api/med_files_api/settings.py
+++ med_files_api/med_files_api/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Third party
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
     # Local
     'core',
+    'users',
     'medicines',
     'doctors',
 ]
