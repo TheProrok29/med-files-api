754f6060e9214e47ca7659e5dab9b34b86c5559e
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-16 13:18:21 +0200

    Add CreateTokenView in user views.py

--- med_files_api/user/views.py
+++ med_files_api/user/views.py
@@ -1,10 +1,18 @@
-from rest_framework.mixins import CreateModelMixin
-from rest_framework import viewsets
-from .serializers import UserSerializer
+from rest_framework import mixins, viewsets
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from core.models import User
 
+from .serializers import UserSerializer, AuthTokenSerializer
 
-class CreateUserView(CreateModelMixin, viewsets.GenericViewSet):
+
+class CreateUserView(mixins.CreateModelMixin, viewsets.GenericViewSet):
     """Create a new user in the system"""
     model = User
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
