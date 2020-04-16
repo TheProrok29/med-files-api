9757d6b872e03447bd69b20198124d1c541b0652
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-16 12:26:17 +0200

    Add serializers and view for user app.

--- med_files_api/user/serializers.py
+++ med_files_api/user/serializers.py
@@ -9,3 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ('id', 'email', 'password', 'name')
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+
+    def create(self, validated_data):
+        """Create a new user with encrypted password and return it"""
+        return get_user_model().objects.create_user(**validated_data)
--- med_files_api/user/views.py
+++ med_files_api/user/views.py
@@ -1,3 +1,10 @@
-# from django.shortcuts import render
+from rest_framework.mixins import CreateModelMixin
+from rest_framework import viewsets
+from .serializers import UserSerializer
+from core.models import User
 
-# Create your views here.
+
+class CreateUserView(CreateModelMixin, viewsets.GenericViewSet):
+    """Create a new user in the system"""
+    model = User
+    serializer_class = UserSerializer
