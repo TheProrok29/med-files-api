17966d64b9f73796e801a8e6b034cbd459603a49
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-19 18:49:39 +0200

    Use status from DRF in medicine api test and refactor URL

--- med_files_api/medicine/tests/tests_api.py
+++ med_files_api/medicine/tests/tests_api.py
@@ -1,20 +1,21 @@
 from medicine.models import Medicine
 from rest_framework.test import APITestCase
+from rest_framework import status
+
+MEDICINE_URL = 'http://127.0.0.1:8000/api/medicine/'
 
 
 class MedicinesApiTest(APITestCase):
     def test_medicines_get_method(self):
-        url = 'http://127.0.0.1:8000/api/medicine/'
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+        response = self.client.get(MEDICINE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_medicines_post_method(self):
-        url = 'http://127.0.0.1:8000/api/medicine/'
         data = {
             'name': 'New one',
             'description': 'This is the best pain killer',
             'med_form': Medicine.MedicineForm.GLOBULES,
             'med_type': Medicine.MedicineType.VITAMIN,
         }
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, 201)
+        response = self.client.post(MEDICINE_URL, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
