549c6476909a53ce5fb1e68861cd1d9ce227d97e
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-20 10:47:22 +0200

    Create new test to check doctor model creation
    * Add name field to search in user admin page

--- med_files_api/core/admin.py
+++ med_files_api/core/admin.py
@@ -24,7 +24,7 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('email', 'password1', 'password2')
         }),
     )
-    search_fields = ('email',)
+    search_fields = ('email', 'name')
 
 
 admin.site.register(User, UserAdmin)
--- med_files_api/doctor/tests/tests_model.py
+++ med_files_api/doctor/tests/tests_model.py
@@ -3,10 +3,20 @@ from doctor.models import Doctor
 
 
 class DoctorModelTest(TestCase):
-    def test_doctor_creation(self):
+    def test_create_doctor_with_full_data(self):
+        """Test creating a new doctor"""
+
         doctor = Doctor.objects.create(name='Tom Hardy',
                                        adres='Warsaw, Chrobrego 25/4a',
                                        phone_number='765789432',
                                        doc_type=Doctor.DoctorSpecialization.SURGEON,)
         self.assertTrue(isinstance(doctor, Doctor))
         self.assertEqual(doctor.__str__(), doctor.name)
+
+    def test_create_doctor_with_minimum_data(self):
+        """Test creating a new doctor with minimum data; doc_type is mandatory
+        but has default value set in model"""
+        name = 'Ward Brankenmister'
+        doctor = Doctor.objects.create(name=name)
+        self.assertTrue(isinstance(doctor, Doctor))
+        self.assertEqual(doctor.name, name)
