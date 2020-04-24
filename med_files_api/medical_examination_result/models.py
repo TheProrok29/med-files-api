c26e6cbbea0b33d3306e1e086e7d2f8c6d5db27f
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-24 11:47:51 +0200

    Create basic model for medical examination result app
    * Add new migration

--- med_files_api/medical_examination_result/models.py
+++ med_files_api/medical_examination_result/models.py
@@ -1,3 +1,14 @@
-# from django.db import models
+from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class MedicalExaminationResult(models.Model):
+    """"Model using to store medical examination result object"""
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    description = models.TextField(verbose_name='Description')
+    add_date = models.DateTimeField(auto_now_add=True)
+    date_of_exam = models.DateTimeField()
+
+    def __str__(self):
+        return self.description[:100]
