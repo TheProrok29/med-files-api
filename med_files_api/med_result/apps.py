bf2962d8f3df3c78306542d772667e038a0e92c5
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-25 19:43:14 +0200

    Create new app med_result to migrate from to long app name
    medical_examination_result

---
+++ med_files_api/med_result/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MedResultConfig(AppConfig):
+    name = 'med_result'
---
+++ med_files_api/med_result/tests/tests_med_exam_results_api.py
@@ -0,0 +1,14 @@
+# from django.test import TestCase
+# from django.urls import reverse
+# from rest_framework import status
+# from rest_framework.test import APIClient
+# from result.serializers import MedicalExaminationResultSerializer
+# import tempfile
+# import os
+# from PIL import Image
+
+
+# MED_EXAM_RESULT_URL = reverse('medical_examination_result:medical_examination_result-list')
+
+
+# def image_upload_url()
