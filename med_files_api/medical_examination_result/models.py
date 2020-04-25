f8375a359164a7fd86daea94a23cc299d2e234d8
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-25 18:30:00 +0200

    Add image field to medical examination model
    * Add new migration

--- med_files_api/medical_examination_result/models.py
+++ med_files_api/medical_examination_result/models.py
@@ -1,5 +1,15 @@
+import uuid
+import os
 from django.db import models
 from django.conf import settings
+
+
+def exam_result_file_path(instance, filename):
+    """Generate file path for new exam result image"""
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('uploads/examination_result/', filename)
 
 
 class MedicalExaminationResult(models.Model):
@@ -9,6 +19,7 @@ class MedicalExaminationResult(models.Model):
     description = models.TextField(verbose_name='Description')
     add_date = models.DateTimeField(auto_now_add=True)
     date_of_exam = models.DateTimeField()
+    image = models.ImageField(null=True, upload_to=exam_result_file_path)
 
     def __str__(self):
         return self.description[:100]
