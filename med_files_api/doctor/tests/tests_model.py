ec61485ed63ab5df063a6b00e04286e84a7f394a
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-27 19:12:21 +0200

    Uptade doctor model test

--- med_files_api/doctor/tests/tests_model.py
+++ med_files_api/doctor/tests/tests_model.py
@@ -1,20 +1,27 @@
 from django.test import TestCase
-from ..models import Doctor
+from ..models import Doctor, DoctorSpecialization
+from django.contrib.auth import get_user_model
+
+
+class DoctorSpecializationModelTest(TestCase):
+    def test_create_new_spacialziation(self):
+        """Test creating a new doctor specialization"""
+        doc_specialization = DoctorSpecialization.objects.create(specialization='Neurologist')
+        self.assertTrue(isinstance(doc_specialization, DoctorSpecialization))
+        self.assertEqual(str(doc_specialization), 'Neurologist')
 
 
 class DoctorModelTest(TestCase):
     def test_create_doctor_with_full_data(self):
         """Test creating a new doctor"""
-        doctor = Doctor.objects.create(name='Tom Hardy',
+        user = get_user_model().objects.create_user(
+            email='test@something.com',
+            password='Testpassword123')
+        doc_spec = DoctorSpecialization.objects.create(specialization='Neurologist')
+        doctor = Doctor.objects.create(user=user,
+                                       name='Tom Hardy',
                                        adres='Warsaw, Chrobrego 25/4a',
                                        phone_number='765789432',
-                                       doc_spec=Doctor.DoctorSpecialization.SURGEON,)
+                                       doc_spec=doc_spec)
         self.assertTrue(isinstance(doctor, Doctor))
         self.assertEqual(doctor.__str__(), doctor.name)
-
-    def test_create_doctor_with_minimum_data(self):
-        """Test creating a new doctor with minimum data; doc_type is mandatory
-        but has default value set in model"""
-        name = 'Ward Brankenmister'
-        doctor = Doctor.objects.create(name=name)
-        self.assertEqual(doctor.name, name)
