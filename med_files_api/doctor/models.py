47d5a92b3ff33733919b29c5aff131bf81f2214d
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-27 18:40:39 +0200

    Update doctor model
    * add user foreign key
    * add DoctorSpecialization class
    * add doc_spec foreign key

--- med_files_api/doctor/models.py
+++ med_files_api/doctor/models.py
@@ -1,34 +1,26 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+
+
+class DoctorSpecialization(models.Model):
+    """DoctorSpecialization model that be using during creating a new doctor"""
+    specialization = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.specialization
 
 
 class Doctor(models.Model):
-    class DoctorSpecialization(models.TextChoices):
-        UROLOGIS = 'URO', _('Urologis')
-        ORTHOPEDIST = 'ORT', _('Orthopedist')
-        OPHTHALMOLOGIST = 'OPH', _('Ophthalmologist')
-        NEUROLOGIST = 'NEU', _('Neurologist')
-        SURGEON = 'SUR', _('Surgeon')
-        LARYNGOLOGIST = 'LAR', _('Laryngologist')
-        GYNECALOGIST = 'GYN', _('Gynecologist')
-        FAMILY_DOCTOR = 'FDO', _('Family doctor')
-        CARDIOLOGIST = 'CAR', _('Cardiologist')
-        ONCOLOGIST = 'ONC', _('Oncologist')
-        GASTROENTEROLOGIST = 'GAS', _('Gastroenterologist')
-        ENDYCORNOLOGIST = 'END', _('Endocrinologist.')
-        DERMATOLOGIST = 'DER', _('Dermatologist')
-        ALLERGIST = 'ALL', _('Allergist')
-
+    """"Model using to store medical examination result object"""
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(verbose_name='Name', max_length=120, unique=True)
     adres = models.CharField(verbose_name='Adres', max_length=200, blank=True, null=True)
 
     phone_number = models.CharField(
         verbose_name='Phone Number', max_length=12, unique=True, blank=True, null=True)
 
-    doc_spec = models.CharField(
-        verbose_name='Specialization', max_length=3,
-        choices=DoctorSpecialization.choices,
-        default=DoctorSpecialization.FAMILY_DOCTOR)
+    doc_spec = models.ForeignKey('DoctorSpecialization', verbose_name='Specialization', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
