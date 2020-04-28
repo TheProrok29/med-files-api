f26500f1843413c9c0f404546c39f9f0b3cbd3d9
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-28 17:58:02 +0200

    Add authentication and permission to DoctorViewSet
    * show only doctors who were created by this user

--- med_files_api/doctor/views.py
+++ med_files_api/doctor/views.py
@@ -1,9 +1,17 @@
-from rest_framework import viewsets
-
+from rest_framework import viewsets, authentication, permissions
 from .models import Doctor
 from .serializers import DoctorSerializer
 
 
 class DoctorViewSet(viewsets.ModelViewSet):
     serializer_class = DoctorSerializer
-    queryset = Doctor.objects.all()
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        """This view return a list of all doctors for the currently authenticated user."""
+        user = self.request.user
+        return Doctor.objects.filter(user=user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
