a11f39a378151d005cea58ff1ce4437f4e8db8d2
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-04-28 17:55:12 +0200

    Change doctor model field name and fields option
    * Add migrations

--- med_files_api/doctor/models.py
+++ med_files_api/doctor/models.py
@@ -5,10 +5,10 @@ from django.conf import settings
 
 class DoctorSpecialization(models.Model):
     """DoctorSpecialization model that be using during creating a new doctor"""
-    specialization = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        return self.specialization
+        return self.name
 
 
 class Doctor(models.Model):
@@ -18,9 +18,9 @@ class Doctor(models.Model):
     adres = models.CharField(verbose_name='Adres', max_length=200, blank=True, null=True)
 
     phone_number = models.CharField(
-        verbose_name='Phone Number', max_length=12, unique=True, blank=True, null=True)
+        verbose_name='Phone Number', max_length=12, blank=True, null=True)
 
-    doc_spec = models.ForeignKey('DoctorSpecialization', verbose_name='Specialization', on_delete=models.CASCADE)
+    specialization = models.ForeignKey('DoctorSpecialization', verbose_name='Specialization', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
