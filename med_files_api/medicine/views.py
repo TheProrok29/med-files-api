1295bd4f8f1d58d1e90da1e3ddf43f08dd1c2bf2
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-05-03 17:50:26 +0200

    Add SessionAuthentication to MedicineViewSet

--- med_files_api/medicine/views.py
+++ med_files_api/medicine/views.py
@@ -7,7 +7,7 @@ from .serializers import MedicineSerializer
 class MedicineViewSet(viewsets.ModelViewSet):
     serializer_class = MedicineSerializer
     queryset = Medicine.objects.all()
-    authentication_classes = (authentication.TokenAuthentication,)
+    authentication_classes = (authentication.TokenAuthentication, authentication.SessionAuthentication)
     permission_classes = (permissions.IsAuthenticated,)
 
     def get_queryset(self):
@@ -15,5 +15,5 @@ class MedicineViewSet(viewsets.ModelViewSet):
         return self.queryset.filter(user=self.request.user)
 
     def perform_create(self, serializer):
-        """Automatically add the user to the created medicine"""
+        """Automatically add the user to the medicine"""
         serializer.save(user=self.request.user)
