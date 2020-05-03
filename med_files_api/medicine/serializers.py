9a10bc79c28a3ca6b7b64f87fa82e704cb105cc4
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-05-03 17:49:59 +0200

    Add UniqueTogetherValidator to Medicine serializer
    and pirmary key related field user

--- med_files_api/medicine/serializers.py
+++ med_files_api/medicine/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
-
+from rest_framework.validators import UniqueTogetherValidator
 from .models import Medicine
 
 
 class MedicineSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(
+        read_only=True, default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Medicine
-        fields = ('id', 'name', 'description', 'med_form', 'med_type')
+        fields = ('id', 'name', 'user', 'description', 'med_form', 'med_type')
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Medicine.objects.all(),
+                fields=['user', 'name']
+            )
+        ]
