26f7eaa73e38f2d9fed03660dce70b7391fa971f
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-05-05 19:04:07 +0200

    Temporary comment unused import in Visit app

--- med_files_api/visit/models.py
+++ med_files_api/visit/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+# from django.db import models
+# from django.conf import settings
+# from medicine.models import Medicine
+# from doctor.models import Doctor
+# from med_result.models import MedResult
 
-# Create your models here.
+
+# class Visit(models.Model):
+#     """A model that represents a medical visit and integrates
+#     most of the other modules"""
+
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     visit_date = models.DateTimeField()
+#     doctor = models.ForeignKey(Doctor)
+#     medicine = models.ForeignKey(Medicine, blank=True, null=True)
+#     med_result = models.ForeignKey(MedResult, blank=True, null=True)
