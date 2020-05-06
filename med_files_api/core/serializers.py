1075c4711005515edcad320968e612cc5cdb3702
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-05-06 17:20:03 +0200

    Create basic serializer for Tag objects

---
+++ med_files_api/core/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Tag
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for Tag objects"""
+
+    class Meta:
+        model = Tag
+        fields = ('id', 'name')
+        red_only_fields = ('id',)
