e3e3da8d42339ab1577f8a9c7532098742d83b9a
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-05-06 17:44:31 +0200

    Add failing tests for creating Tag using Tag API endpoint

--- med_files_api/core/tests/test_tag_api.py
+++ med_files_api/core/tests/test_tag_api.py
@@ -45,3 +45,21 @@ class PrivateTagApiTests(APITestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tag(self):
+        """Test creating a new tag"""
+        payload = {'name': 'Test tag'}
+        res = self.client.post(TAG_URL, payload)
+
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        )
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        """Test creating a new tag with invalid payload"""
+        payload = {'name': ''}
+        res = self.client.post(TAG_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
