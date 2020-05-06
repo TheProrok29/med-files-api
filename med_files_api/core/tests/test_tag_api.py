9fd15cb75cd2c5ca07e74a47e2ea9ae9b452bfe4
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-05-06 17:05:18 +0200

    Add failing tests for Tag future API

---
+++ med_files_api/core/tests/test_tag_api.py
@@ -0,0 +1,47 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from ..models import Tag
+from ..serializers import TagSerializer
+
+
+TAG_URL = reverse('api:tag-list')
+
+
+class PublicTagApiTests(APITestCase):
+    """Test the publicly avaialble tags API"""
+
+    def test_login_required(self):
+        """Test that login is required for retrieving tags"""
+        res = self.client.get(TAG_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateTagApiTests(APITestCase):
+    """Test the authorized user tags API"""
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user('prorok@vp.pl', 'testpass')
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_tags(self):
+        """Test retrieving tags"""
+        Tag.objects.create(user=self.user, name='Heart')
+        Tag.objects.create(user=self.user, name='Head')
+        tags = Tag.objects.all().order_by('-name')
+        serializer = TagSerializer(tags, many=True)
+
+        res = self.client.get(TAG_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_tag_limited_to_user(self):
+        """Test that tags returned are for the authenticated user"""
+        new_user = get_user_model().objects.create_user('lambado2@gmail.com', 'hardpassevermade')
+        Tag.objects.create(user=new_user, name='Leg')
+        tag = Tag.objects.create(user=self.user, name='Head')
+        res = self.client.get(TAG_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], tag.name)
