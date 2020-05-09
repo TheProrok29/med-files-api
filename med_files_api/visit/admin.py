5cc39ab8e49b51dd82fbd518ebbe0cad7a0aaaa4
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-05-09 11:51:10 +0200

    Register visit model in django admin

--- med_files_api/visit/admin.py
+++ med_files_api/visit/admin.py
@@ -1,1 +1,11 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Visit
+
+
+class VisitAdmin(admin.ModelAdmin):
+    ordering = ['id']
+    list_display = ['name', 'doctor', 'visit_date', 'user']
+    search_fields = ('name', 'doctor')
+
+
+admin.site.register(Visit, VisitAdmin)
