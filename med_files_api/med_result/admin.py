81ea095780bb2f4cbcba591f63ae4dace1ec8e24
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-05-17 18:26:53 +0200

    Register med_image in admin and add connected image to
    med result list_display

--- med_files_api/med_result/admin.py
+++ med_files_api/med_result/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 
-from .models import MedResult
+from .models import MedResult, MedImage
 
 
 class MedResultAdmin(admin.ModelAdmin):
+    model = MedResult
     ordering = ['id']
-    list_display = ['name', 'user', 'add_date']
+    list_display = ['name', 'user', 'add_date', 'med_image']
     search_fields = ('name', 'user')
+
+    def med_image(self, obj):
+        return MedImage.objects.filter(med_result=obj)
 
 
 admin.site.register(MedResult, MedResultAdmin)
+admin.site.register(MedImage)
