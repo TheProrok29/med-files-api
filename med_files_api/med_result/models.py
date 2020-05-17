07e7e9a01061523478300c2f49df0ee641a056bf
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-05-17 18:26:04 +0200

    Split med_result image to different model
    * add migrations

--- med_files_api/med_result/models.py
+++ med_files_api/med_result/models.py
@@ -21,9 +21,18 @@ class MedResult(models.Model):
     name = models.CharField(verbose_name='Name', max_length=255)
     description = models.TextField(verbose_name='Description', blank=True)
     add_date = models.DateTimeField(auto_now_add=True)
-    date_of_exam = models.DateField()
-    image = models.ImageField(null=True, blank=True, upload_to=med_result_file_path)
+    date_of_exam = models.DateField(null=True, blank=True)
     tag = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MedImage(models.Model):
+    """Model using to collect medical result image"""
+    name = models.CharField(verbose_name='Name', max_length=255)
+    image = models.ImageField(null=True, blank=True, upload_to=med_result_file_path)
+    med_result = models.ForeignKey(MedResult, related_name='result', null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
